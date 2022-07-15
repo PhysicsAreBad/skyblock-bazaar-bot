46751ad55799784de26a4e9dd7b7a42f107c9416
physicsarebad <8169202+PhysicsAreBad@users.noreply.github.com>
2022-07-14 23:30:43 -0400

    New web server design

--- src/webServer.ts
+++ src/webServer.ts
@@ -3,16 +3,10 @@
 import http from 'http'
 
 export default function startServer () {
-    const host = 'localhost';
-    const port = process.env.PORT as unknown as number || 5000;
-
     const requestListener : http.RequestListener = function (req, res) {
         res.writeHead(200);
         res.end("go away");
     };
 
-    const server = http.createServer(requestListener);
-    server.listen(port, host, undefined, () => {
-        console.log(`Server is running on http://${host}:${port}`);
-    });
+    const server = http.createServer(requestListener).listen(process.env.PORT as unknown as number || 5000);
 }
